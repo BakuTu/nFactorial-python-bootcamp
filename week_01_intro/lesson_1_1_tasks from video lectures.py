0c6e7253656b9c751de5e5c0294c34b4a268d497
Bahitzan Tulegenov <tulegenovabylay@gmail.com>
2025-08-06 21:30:32 +0500

    Add lesson1_1_List practice

---
+++ week_01_intro/lesson_1_1_tasks from video lectures.py
@@ -0,0 +1,4 @@
+# Date: 2025-08-06
+
+x = int(input("Enter a number: "))  # Ask the user to enter a number and convert it from string to integer
+print(x % 2 == 0)   # Print True if the number is even (no remainder when divided by 2), otherwise False
