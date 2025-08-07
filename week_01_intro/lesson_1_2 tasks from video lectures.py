cbc175d7b5fd52931f0f2083433bb5b9491b0850
Bahitzan Tulegenov <tulegenovabylay@gmail.com>
2025-08-07 09:40:16 +0500

    Add lesson 1_2_List practice

---
+++ week_01_intro/lesson_1_2 tasks from video lectures.py
@@ -0,0 +1,13 @@
+# Date: 2025-08-07
+
+x = 738 # Assign a three-digit number to variable x
+b = x % 10 # Get the last digit (ones place)
+
+c = x // 100    # Get the first digit (hundreds place)
+
+d = (x // 10) % 10  # Get the middle digit (tens place)
+
+print(b + c + d)    # Print the sum of all three digits
+
+
+
