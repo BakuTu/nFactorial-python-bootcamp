9236cfb44c29ef759c9f457afd2f471ac9947b88
Bahitzan Tulegenov <tulegenovabylay@gmail.com>
2025-08-10 15:03:54 +0500

    Add lesson_1_taks_2_List practice

---
+++ week_01_intro/lesson_1_task_2: Reverse a Three Digit Number.py
@@ -0,0 +1,5 @@
+# Date: 2025-08-10
+# Reverse a Three Digit Number
+
+number_string = input() # Set input from the user as a string
+print(number_string[::-1])  # Reverse the string using slice [:: -1])
